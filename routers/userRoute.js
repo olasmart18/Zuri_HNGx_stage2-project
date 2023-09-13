e5a8eb7ede5dc3200230be85e7142103ffc18c88
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-09-13 11:12:55 -0700

    connect to cloud database

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { newUser, 
-         getSingleUser,
-        updateUser,
-        deleteUser } from '../controllers/userController.js'
-const router = express.Router()
+import {
+  newUser,
+  getSingleUser,
+  updateUser,
+  deleteUser
+} from '../controllers/userController.js';
+const router = express.Router();
 
 router.post('/', newUser);
-router.get('/:user_id',getSingleUser)
-router.put('/:user_id', updateUser)
-router.delete('/:user_id', deleteUser)
+router.get('/:user_id', getSingleUser);
+router.put('/:user_id', updateUser);
+router.delete('/:user_id', deleteUser);
 
 export default router;
--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,31 @@
 import mongoose from 'mongoose';
+// import { MongoClient } from 'mongodb';
 
+// localhost database connection
 const dbConnection = async () => {
-    const dbName = 'User_DB';
-    const url = `${process.env.MONGO_URI}/${dbName}`
-    const options = {
-        useNewUrlParser: false,
-        useUnifiedTopology: false
-        };
+  const dbName = 'User_DB';
+  const url = `${process.env.MONGO_URI}/${dbName}`;
+  const options = {
+    useNewUrlParser: false,
+    useUnifiedTopology: false
+  };
 
-        await mongoose.connect(url, options)
-        .then(() => {
-                console.log(`connected to ${url}`);
-        }).catch((err)=> {
-            console.log(err);
-        })
+  await mongoose.connect(url, options)
+    .then(() => {
+      console.log(`connected to ${url}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+};
+
+// cloud database connection
+async function run () {
+  try {
+    const uri = process.env.MONGO_URI_ATLAS;
+    await mongoose.connect(uri);
+    console.log('You successfully connected to MongoDB!');
+  } catch (err) {
+    console.log(err);
+  }
 }
-export default dbConnection;
+export default run;
